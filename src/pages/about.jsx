fce7334d1de8d820d43cef1dc7a11afdd850d5a9
Richard Kwon <rkminjae@gmail.com>
2020-05-27 19:18:37 +1200

    Add string to headerText prop in About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../components/header'
 
 export default function About() {
     return (
         <div style={{ color: `teal` }}>
-            <h1>About Page</h1>
+            <Header headerText="About" />
             <p>This is an about page</p>
         </div>
     )

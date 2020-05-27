afa8bcb011ca0d96d1dbea41a52f7fab0dfa5555
Richard Kwon <rkminjae@gmail.com>
2020-05-27 19:14:29 +1200

    Create About component page

---
+++ src/pages/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div style={{ color: `teal` }}>
+            <h1>About Page</h1>
+            <p>This is an about page</p>
+        </div>
+    )
+}

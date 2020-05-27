21b56eaa081a1eb66da5228ed5a3c14014cbe691
Richard Kwon <rkminjae@gmail.com>
2020-05-27 19:08:40 +1200

    Add h1 an p tags to div in Home component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div style={{ color: `purple` }}>
-    Hello Gatsby!
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>The start of Gatsby development</p>
     </div>
+  )
 }

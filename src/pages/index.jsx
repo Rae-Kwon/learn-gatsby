35e770ba32c32da603f993862983410183f1c366
Richard Kwon <rkminjae@gmail.com>
2020-05-27 19:11:14 +1200

    Add an image to Home component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div style={{ color: `purple` }}>
       <h1>Hello Gatsby!</h1>
       <p>The start of Gatsby development</p>
+      <img src="https://images.unsplash.com/photo-1590493139659-9601ac3ba0bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="Sunset"/>
     </div>
   )
 }

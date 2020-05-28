5755a2fa3ac93f26ea56a5fa8f1bbcd008b90b62
Richard Kwon <rkminjae@gmail.com>
2020-05-28 18:49:59 +1200

    Change rendered content in index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,13 @@
 import React from "react"
-import { Link } from 'gatsby'
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <h1>Hello Gatsby!</h1>
-      <p>The start of Gatsby development</p>
-      <img src="https://images.unsplash.com/photo-1590493139659-9601ac3ba0bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="Sunset"/>
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
     </div>
   )
 }

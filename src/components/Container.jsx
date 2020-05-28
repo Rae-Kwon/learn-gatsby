fd29513612a1940666ff091429661f814aaf1637
Richard Kwon <rkminjae@gmail.com>
2020-05-28 18:37:23 +1200

    Create Container component

---
+++ src/components/Container.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import containerStyles from './Container.modules.css'
+
+export default function Container ({ children }) {
+    return <div className={containerStyles.container}>{children}</div>
+}

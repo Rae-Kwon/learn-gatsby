901d780883614d822d46208d20fdc9535d2402bb
Richard Kwon <rkminjae@gmail.com>
2020-05-28 18:54:04 +1200

    Reformat code

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`
-      }
-    }
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
   ],
 }

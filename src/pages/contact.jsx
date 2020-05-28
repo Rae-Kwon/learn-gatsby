42264311f3fa51fed943a3c6df6408795901177f
Richard Kwon <rkminjae@gmail.com>
2020-05-28 18:55:15 +1200

    Change contents of contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,13 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/Header"
 
 export default function Contact() {
   return (
     <div>
-      <Link to="/">Home</Link>
-      <Header headerText="Contact" />
-      <p>Send a message to me!!</p>
+      <h1>I'd love to talk! Email me at the address below</h1>
+      <p>
+        <a href="mailto:me@example.com">me@example.com</a>
+      </p>
     </div>
   )
 }
